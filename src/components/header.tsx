31a33f90db3b4a6635a0d8bf17b487919e8c12b3
kamiviolet <kamiviolet@gmail.com>
2023-06-27 14:11:20 +0100

    add header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,15 +2,11 @@ import { Address } from "@/types";
 
 export default function Header({address}: {address: Address | null}) {
     return (
-        <div className="my-10">
-            <h1 className="text-6xl small-caps text-center pt-[3em] pb-[1em]">
+        <div className="z-20 py-14">
+            <h1 className="text-7xl tracking-wider uppercase font-extrabold font-serif text-center leading-loose">
                 { address?.country || 'Weathering' }
             </h1>
-            {
-                address
-                ? <></>
-                : <p>Interested in knowing the weather in somewhere else? You may want to manually set location in the search engine at the top.</p>
-            }
+            <p className="text-xl leading-loose">Interested in knowing the weather in somewhere else? You may want to manually set location in the search engine at the top.</p>
         </div>
     )
 }
