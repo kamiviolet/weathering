d0c803cacb229c0d92c5093556a3d135f9b1e6a7
kamiviolet <kamiviolet@gmail.com>
2023-06-27 14:10:25 +0100

    rename utils to api

--- src/api.ts
+++ src/api.ts
@@ -4,12 +4,17 @@ import { Location } from "@/types"
 export function getForecast(location: Location) {
     return axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${location.latitude}&lon=${location.longitude}&appid=${process.env.OPENWEATHER_API}`)
         .then(({data}) => data)
-        .catch(error => console.warn(error))
+        .catch((error) => console.warn(error))
 }
 
 export function getAddress(location: Location) {
     return axios.get(`https://api.geoapify.com/v1/geocode/reverse?lat=${location.latitude}&lon=${location.longitude}&apiKey=${process.env.GEOAPIFY_API}`)
         .then(({data}) => data.features[0].properties)
-        .catch(error => console.warn(error))
+        .catch((error) => console.warn(error))
 }
 
+export function getBackground(city: string) {
+    return axios.get(`https://api.unsplash.com/photos/random?query=${city}&orientation=landscape&client_id=WSrJzG3Coskvd68SGBx0FnOMmfXMLQk31weuouHifFg`)
+        .then(({data}) => data)
+        .catch((error) => console.warn(error))
+}
