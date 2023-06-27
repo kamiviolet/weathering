c07f7b178f12312972dcb22586bd9e64f588058f
kamiviolet <kamiviolet@gmail.com>
2023-06-27 14:10:41 +0100

    add utils for utility functions

--- src/utils.ts
+++ src/utils.ts
@@ -2,14 +2,19 @@ import { Weather } from "@/types";
 
 export function formatForecastResponse(day:{[key:string]:any}, i:number):(Weather|undefined) {
   const clone = {
+    dt: day.dt,
     main: day.weather[0].main,
-    temp: day.main.temp,
-    feels_like: day.main.feels_like,
-    temp_min: day.main.temp_min,
-    temp_max: day.main.temp_max,
+    temp: convertKevinToCelcius(day.main.temp),
+    feels_like: convertKevinToCelcius(day.main.feels_like),
+    temp_min: convertKevinToCelcius(day.main.temp_min),
+    temp_max: convertKevinToCelcius(day.main.temp_max),
     humidity: day.main.humidity,
     wind_speed: day.wind.speed,
     time: day.dt_txt
   }
   return clone;
+}
+
+export function convertKevinToCelcius(degree: number):number {
+  return +(degree -  273.15).toFixed(2);
 }
