115c20648cfa1b395cbacaff15800302b2fe9ffa
kamiviolet <kamiviolet@gmail.com>
2023-06-28 13:19:37 +0100

    update types

--- src/types.ts
+++ src/types.ts
@@ -4,23 +4,34 @@ export type WeatherSource = {
 
 export type Location = {
   latitude: number,
-  longitude: number
+  longitude: number,
+  city?: string,
 }
 
 export type Address = {
   addressLine1: string,
   addressLine2: string,
+  city?: string,
   country: string,
 }
 
 export type Weather = {
-    main: string;
-    temp: number;
-    feels_like: number;
-    temp_min: number;
-    temp_max: number;
-    humidity: number;
-    wind_speed: number;
-    time: string;
-    [key:string]: any;
-  };
+    main: string,
+    temp: number,
+    feels_like: number,
+    temp_min: number,
+    temp_max: number,
+    humidity: number,
+    wind_speed: number,
+    time: string,
+    [key:string]: any,
+  }
+
+export type searchResult = {
+  city: string,
+  country: string,
+  place_id: number,
+  latitude: number,
+  longitude: number,
+  [key:string]: any,
+}
