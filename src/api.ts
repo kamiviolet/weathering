f6fb0ce02c1c4ed7f3c2d3036730d29934548308
kamiviolet <kamiviolet@gmail.com>
2023-06-28 13:21:17 +0100

    refactor api fetch

--- src/api.ts
+++ src/api.ts
@@ -4,17 +4,23 @@ import { Location } from "@/types"
 export function getForecast(location: Location) {
     return axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${location.latitude}&lon=${location.longitude}&appid=${process.env.OPENWEATHER_API}`)
         .then(({data}) => data)
-        .catch((error) => console.warn(error))
+        .catch((error:Error) => console.warn(error))
 }
 
 export function getAddress(location: Location) {
     return axios.get(`https://api.geoapify.com/v1/geocode/reverse?lat=${location.latitude}&lon=${location.longitude}&apiKey=${process.env.GEOAPIFY_API}`)
         .then(({data}) => data.features[0].properties)
-        .catch((error) => console.warn(error))
+        .catch((error:Error) => console.warn(error))
 }
 
 export function getBackground(city: string) {
-    return axios.get(`https://api.unsplash.com/photos/random?query=${city}&orientation=landscape&client_id=WSrJzG3Coskvd68SGBx0FnOMmfXMLQk31weuouHifFg`)
+    return axios.get(`https://api.unsplash.com/photos/random?query=${city}&orientation=landscape&client_id=${process.env.UPSPLASH_API}`)
         .then(({data}) => data)
-        .catch((error) => console.warn(error))
+        .catch((error:Error) => console.warn(error))
+}
+
+export function getAutocomplete(letters: null|undefined|string) {
+    return axios.get(`https://api.geoapify.com/v1/geocode/autocomplete?text=${letters}&apiKey=${process.env.GEOAPIFY_API}`)
+        .then(({data}) => data)
+        .catch((error:Error) => console.warn(error))
 }
