624a9bb4cd6cc9656f44dd0883b9d3f6a4c78c19
kamiviolet <kamiviolet@gmail.com>
2023-06-29 10:15:37 +0100

    adjust font size

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,10 +3,10 @@ import { Address } from "@/types";
 export default function Header({address}: {address: Address}) {
     return (
         <header className="py-10 px-5">
-            <h1 className="text-6xl tracking-wider uppercase font-extrabold font-serif text-center hyphens-auto leading-loose">
+            <h1 className="text-3xl md:text-6xl tracking-wider uppercase font-extrabold font-serif text-center hyphens-auto leading-loose">
                 { address.city || 'Weathering' }
             </h1>
-            <h2 className="text-4xl tracking-wider uppercase font-extrabold font-serif text-center hyphens-auto leading-regular pb-10">
+            <h2 className="text-xl md:text-4xl tracking-wider uppercase font-extrabold font-serif text-center hyphens-auto leading-regular pb-10">
                 { address?.country || "" }
             </h2>
             <p className="text-xl text-center leading-loose">
